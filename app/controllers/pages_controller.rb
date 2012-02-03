1a332adf86797870cdae366bc2394a0d6ae8f984
Nagesh <nag.501@gmail.com>
2012-02-03 14:19:13 +0530

    Layouts Application

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   def home
-  @title="Instalnce variable"
+  @title="Instalnce variable_home"
   end
-
+  
   def contact
+  @title="Instalnce variable_contact"
   end
 
   def about
+  @title="Instalnce variable_about"
   end
 end
